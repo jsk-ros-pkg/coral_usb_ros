627185154673bb6bcf2c7ea4eb594fa42d7a68e0
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-05-28 11:02:33 +0900

    download segmentation models

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -7,6 +7,13 @@ import urllib
 import rospkg
 
 url = 'https://dl.google.com/coral/canned_models/all_models.tar.gz'
+seg_urls = [
+    'https://github.com/google-coral/edgetpu/raw/master/test_data/'
+    'deeplabv3_mnv2_dm05_pascal_quant_edgetpu.tflite',
+    'https://github.com/google-coral/edgetpu/raw/master/test_data/'
+    'deeplabv3_mnv2_pascal_quant_edgetpu.tflite',
+]
+
 
 rospack = rospkg.RosPack()
 pkg_path = rospack.get_path('coral_usb')
@@ -18,3 +25,8 @@ urllib.urlretrieve(url, tar_path)
 
 with tarfile.open(tar_path) as tar_f:
     tar_f.extractall(models_path)
+
+for seg_url in seg_urls:
+    filename = seg_url.split('/')[-1]
+    filepath = os.path.join(models_path, filename)
+    urllib.urlretrieve(seg_url, filepath)

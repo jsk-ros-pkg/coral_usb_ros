c21fff9c56de1d6553f9affc7271e93703c0a161
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-01-06 20:11:01 +0900

    move human_pose_estimator to python/

--- python/coral_usb/__init__.py
+++ python/coral_usb/__init__.py
@@ -1,4 +1,6 @@
+from .posenet.pose_engine import PoseEngine  # NOQA
+
 from .detector_base import EdgeTPUDetectorBase  # NOQA
 from .face_detector import EdgeTPUFaceDetector  # NOQA
+from .human_pose_estimator import EdgeTPUHumanPoseEstimator  # NOQA
 from .object_detector import EdgeTPUObjectDetector  # NOQA
-from .posenet.pose_engine import PoseEngine  # NOQA

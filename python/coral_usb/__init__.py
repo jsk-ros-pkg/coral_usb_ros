4ada079397eb82647e2de36f92b61613d4840e4a
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-01-06 23:06:07 +0900

    add services

--- python/coral_usb/__init__.py
+++
@@ -1,7 +0,0 @@
-from .posenet.pose_engine import PoseEngine  # NOQA
-
-from .detector_base import EdgeTPUDetectorBase  # NOQA
-from .face_detector import EdgeTPUFaceDetector  # NOQA
-from .human_pose_estimator import EdgeTPUHumanPoseEstimator  # NOQA
-from .object_detector import EdgeTPUObjectDetector  # NOQA
-from .semantic_segmenter import EdgeTPUSemanticSegmenter  # NOQA

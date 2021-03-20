71cdbbcfb67d925cbb1a6c3bf09f5aeec9b3839b
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2021-03-20 22:04:09 +0900

    change for EdgeTPUPanoramaObjectDetector

--- python/coral_usb/object_detector.py
+++ python/coral_usb/object_detector.py
@@ -1,7 +1,6 @@
 import os
 
 from dynamic_reconfigure.server import Server
-import rospkg
 
 from coral_usb.cfg import EdgeTPUObjectDetectorConfig
 from coral_usb.detector_base import EdgeTPUDetectorBase
@@ -10,10 +9,7 @@ from coral_usb.detector_base import EdgeTPUPanoramaDetectorBase
 
 class EdgeTPUObjectDetector(EdgeTPUDetectorBase):
     def __init__(self, namespace='~'):
-        rospack = rospkg.RosPack()
-        pkg_path = rospack.get_path('coral_usb')
-        super(EdgeTPUObjectDetector, self).__init__(
-            namespace)
+        super(EdgeTPUObjectDetector, self).__init__(namespace)
 
         # dynamic reconfigure
         dyn_namespace = namespace
@@ -26,14 +22,7 @@ class EdgeTPUObjectDetector(EdgeTPUDetectorBase):
 
 class EdgeTPUPanoramaObjectDetector(EdgeTPUPanoramaDetectorBase):
     def __init__(self, namespace='~'):
-        rospack = rospkg.RosPack()
-        pkg_path = rospack.get_path('coral_usb')
-        model_file = os.path.join(
-            pkg_path,
-            './models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite')
-        label_file = os.path.join(pkg_path, './models/coco_labels.txt')
-        super(EdgeTPUPanoramaObjectDetector, self).__init__(
-            model_file, label_file, namespace)
+        super(EdgeTPUPanoramaObjectDetector, self).__init__(namespace)
 
         # dynamic reconfigure
         dyn_namespace = namespace

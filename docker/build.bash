a238abdb0a93f8cd4e65716ee541554865cf3d28
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-02-10 13:47:46 +0900

    update docker

--- docker/build.bash
+++ docker/build.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
-[[ ${DOCKER_TAG} =~ ([a-z]+)-([0-9a-z.]+)$ ]]
-ROS_DISTRO=${BASH_REMATCH[1]};
+if [[ $1 = "" ]]; then
+    echo "Please specify ros distro"
+    echo "Usage: build.bash [ROS_DISTRO]"
+    exit 1;
+fi
 
+ROS_DISTRO=$1;
+IMAGE_NAME="knorth55/coral_usb_ros:${ROS_DISTRO}-latest";
 if [[ ${ROS_DISTRO} = "noetic" ]]; then
     UBUNTU_VERSION="focal";
 elif [[ ${ROS_DISTRO} = "melodic" ]]; then
@@ -13,8 +18,13 @@ elif [[ ${ROS_DISTRO} = "kinetic" ]]; then
     UBUNTU_VERSION="xenial";
 elif [[ ${ROS_DISTRO} = "indigo" ]]; then
     UBUNTU_VERSION="trusty";
+else
+    echo "Unsupported distro: $1";
+    exit 1;
 fi
 
 docker build --build-arg ROS_DISTRO=${ROS_DISTRO} \
              --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
              -t ${IMAGE_NAME} .
+
+set +e
